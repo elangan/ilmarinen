93e0ecec094c57767beaed38f3d162b12c2c3841
William Furr <william.furr@gmail.com>
2016-07-07 08:40:48 -0400

    Separate API into separate module, add route for /

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+var api = require('./api');
 var bodyParser = require('body-parser');
 var express = require('express');
 
@@ -10,30 +11,15 @@ app.use(bodyParser.json());
 
 var port = process.env.PORT || 8080;
 
-var router = express.Router();
+app.get('/', function(req, res) {
+  res.send('May the wolves devour the dreamer,'+
+    'Eat the Laplander for dinner,' +
+    'May disease destroy the braggart,' +
+    'Him who said that I should never' +
+    'See again my much-loved home-land,');
+})
 
-// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
-router.get('/', function(req, res) {
-    res.json({ message: 'hooray! welcome to our api!' });   
-});
-
-router.route('inventory')
-  .get(function(req, res) {
-
-  })
-  .post(function(req, res) {
-  
-  });
-
-router.route('jobs')
-  .get(function(req, res) {
-  
-  })
-  .post(function(req, res) {
-  
-  });
-
-app.use('/api', router);
+app.use('/api', api);
 
 app.listen(port);
 console.log('Magic happens on port ' + port);
