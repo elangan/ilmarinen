2a8890769f5d59efb189b64b2e3d1c06a5e57dea
William Furr <william.furr@gmail.com>
2016-07-12 21:38:06 -0400

    only import bought material

--- data/spreadsheet_data_to_csv_import.py
+++ data/spreadsheet_data_to_csv_import.py
@@ -1,32 +1,33 @@
 import csv
 import re
 
+# Imports all purchased materials into the inventory and blueprint tables.
+# Also populates the group table for all items.
+
 def to_float(val):
     return float(re.sub('[^0-9.]', '', val))
 
 def to_int(val):
     return int(re.sub('[^0-9]', '', val))
 
-BLUEPRINT_GROUPS = [
-    'Hybrid Reactions',
-    'Hybrid Component Blueprints',
+# Exclude built items from import
+EXCLUDE_GROUPS = [
     'Tactical Destroyer Blueprints',
     'Strategic Cruiser Blueprints',
-    'Subsystem Blueprints'
+    'Subsystem Blueprints',
+    'Hybrid Tech Component',
+    'Modules and Ammo'
 ]
 
 groups = {}
 inventory_id = 1
 inventory = {}
 blueprints = {}
-job_id = 1
-jobs = {}
-allocations = {}
 with open('spreadsheet_data.csv', 'r') as f:
     reader = csv.DictReader(f)
     for row in reader:
         groups.setdefault(row['Item'], row['Group'])
-        
+
         item_name = inventory.setdefault(row['Item'], {})
         date = item_name.setdefault(row['Date Acquired'], {})
         price = date.setdefault(to_float(row['Unit Price']), {'qty': 0, 'id': inventory_id})
@@ -35,15 +36,9 @@ with open('spreadsheet_data.csv', 'r') as f:
         item = inventory[row['Item']][row['Date Acquired']][to_float(row['Unit Price'])]
         item['qty'] += to_int(row['Qty'])
         
-        if row['Group'] in BLUEPRINT_GROUPS:
+        if row['Group'] in EXCLUDE_GROUPS:
             blueprints[inventory_id] = {'runs': row['Runs'], 'me': row['ME']}
         
-        # TODO fix up input data so there are actually jobs for stuff and there's one job per stuff
-        # TODO clarified collate job data here
-        if row['Job ID'] not in jobs.keys():
-            jobs[row['Job ID']] = {'id': job_id}
-            job_id += 1
-
 with open('item_names.csv', 'w') as f:
     writer = csv.writer(f)
     for item in groups.keys():
@@ -52,18 +47,17 @@ with open('item_names.csv', 'w') as f:
 with open('inventory.csv', 'w') as f:
     writer = csv.writer(f)
     for item_name in inventory.keys():
+        if groups[item_name] in EXCLUDE_GROUPS:
+            continue
         for date in inventory[item_name].keys():
             for price in inventory[item_name][date].keys():
                 item = inventory[item_name][date][price]
+                # Any Hybrid Polymer batch that divides evenly by 10 was built.
+                if groups[item_name] == 'Hybrid Polymers' and item['qty'] % 10 == 0:
+                    continue
                 writer.writerow([item['id'], item_name, item['qty'], price, date])
 
 with open('blueprints.csv', 'w') as f:
     writer = csv.writer(f)
     for id in blueprints.keys():
         writer.writerow([id, blueprints[id]['runs'], blueprints[id]['me']])
-
-with open('jobs.csv', 'w') as f:
-    writer = csv.writer(f)
-    job_id = 1
-    for job in jobs.keys():
-        pass  # TODO collate my data and implement me
