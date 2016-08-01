210248b927ce52e8e98844ef1ed13c46df6170ee
William Furr <william.furr@gmail.com>
2016-07-31 20:00:05 -0400

    Formatting fixes

--- inventory.js
+++ inventory.js
@@ -2,32 +2,28 @@ var sqlite = require('sqlite3').verbose();
 var wait = require('wait.for');
 
 function inventory(db) {
-	this.db_ = db;
-	this.items_ = null;
-	db.all('SELECT inv.id, inv.item_name, inv.unit_price, inv.quantity, ' +
-		          'SUM(COALESCE(alloc.quantity, 0)) AS allocated ' +
-           'FROM inventory AS inv ' +
-           'LEFT JOIN job_allocation AS alloc ON inv.id = alloc.inventory_id ' +
-           'GROUP BY inv.id;',
-           (function(err, rows) {
-           	if (err) throw err;
-           	this.items_ = rows;
-           }).bind(this));
+  this.db_ = db;
+  this.items_ = wait.forMethod(db, 'all',
+    'SELECT inv.id, inv.item_name, inv.unit_price, inv.quantity, ' +
+    'SUM(COALESCE(alloc.quantity, 0)) AS allocated ' +
+    'FROM inventory AS inv ' +
+    'LEFT JOIN job_allocation AS alloc ON inv.id = alloc.inventory_id ' +
+    'GROUP BY inv.id;');
 }
 
 inventory.prototype.getAll = function() {
-	return this.items_;
+  return this.items_;
 };
 
 inventory.prototype.getAvailable = function() {
-	var available = [];
-	for (var i = 0; i < this.items_.length; i++) {
-		var item = this.items_[i];
-		if (item.quantity - item.allocated > 0) {
-			available.push(item);
-		}
-	}
-	return available;
+  var available = [];
+  for (var i = 0; i < this.items_.length; i++) {
+    var item = this.items_[i];
+    if (item.quantity - item.allocated > 0) {
+      available.push(item);
+    }
+  }
+  return available;
 };
 
 inventory.prototype.addItems = function(items, cb) {
