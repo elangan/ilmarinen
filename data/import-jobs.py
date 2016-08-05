957cf53caa47ccdce3f5d17e4d3fcae6a36db0f8
William Furr <william.furr@gmail.com>
2016-08-05 12:01:55 -0400

    Add import jobs script with historical invention jobs

---
+++ data/import-jobs.py
@@ -0,0 +1,24 @@
+import csv
+import re
+import requests
+
+with open('jobs.csv', 'r') as f:
+    reader = csv.DictReader(f)
+    for row in reader:
+	    blueprint = row['Blueprint']
+	    inventory = requests.get('http://localhost:8080/api/inventory').json()
+
+	    # HALP parse inventory response?
+	    oldest_id = None
+	    for item in inventory:
+	    	if item['item_name'] == blueprint:
+	    		if oldest_id is None or oldest_id[0] > item['date_acquired']:
+	    			oldest_id = (item['date_acquired'], item['id'])
+
+	    job = {
+	    	'blueprint_id': oldest_id[1],
+	    	'runs': row['Runs'],
+	    	'start_date': row['Start Date'],
+	    	'output_type': row['Output Type']
+	    }
+	    resp = requests.post('http://localhost:8080/api/jobs', json=job)
