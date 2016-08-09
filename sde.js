734a87c278bcb9b87d7751e6d5d74d86647427a6
William Furr <william.furr@gmail.com>
2016-08-09 11:35:47 -0400

    Added blueprint lookup with typeID to name replacement

--- sde.js
+++ sde.js
@@ -20,4 +20,23 @@ sde.getItemTypeAndGroup = function(item_name) {
   return { 'typeid': typeid, 'group': group };
 };
 
+sde.getBlueprintData = function(blueprint_name) {
+  var typeid = sde.NAMES_TO_TYPE_IDS[blueprint_name];
+  var blueprint_data = sde.BLUEPRINTS[typeid];
+  blueprint_data.blueprint_group = sde.GROUPS[sde.TYPES[typeid].groupID].name;
+  var addTypeNames = function(obj) {
+    for (var key in obj) {
+      if (obj.hasOwnProperty(key)) {
+        if (key === 'typeID') {
+          obj.name = sde.TYPES[obj[key]].name;
+        } else if (typeof obj[key] === 'object') {
+          addTypeNames(obj[key]);
+        }
+      }
+    }
+  };
+  addTypeNames(blueprint_data);
+  return blueprint_data;
+};
+
 module.exports = sde;
